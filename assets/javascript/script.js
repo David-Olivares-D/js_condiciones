494b49cf51f7d16cecf2a92b2c63852d0557d8a0
David-Olivares-D <david.a.olivares@gmail.com>
2024-04-09 14:06:18 -0400

    cambio en el ejercicio 1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,10 +4,10 @@ let guitarra = document.querySelector(".guitarra")
 
 
 guitarra.addEventListener("click", () => {
-    if (guitarra.style.border === "none") {
+    if (guitarra.style.border === "") {
         guitarra.style.border = "2px solid red";
     } else {
-        guitarra.style.border = "none";
+        guitarra.style.border = "";
     }
 });
 
